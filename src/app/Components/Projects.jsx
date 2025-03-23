986358a10818f0e97b7b799c6338e400f8812faf
Jitendra120901 <jitendrachoudhary2729@gmail.com>
2025-03-23 19:13:26 +0530

    write project type

--- src/app/Components/Projects.jsx
+++ src/app/Components/Projects.jsx
@@ -181,7 +181,8 @@ const Projects = () => {
                  as="button"
                  className="px-4 py-2 rounded-xl bg-black dark:bg-white dark:text-black text-white text-xs font-bold"
                >
-                 {/* GitHub */}
+                {item.type}
+
                </CardItem>
              </div>
            </CardBody>
